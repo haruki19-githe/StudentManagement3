e1698f7bf95d76f9aec476eb71aed69973e36787
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-13 12:13:28 +0900

    20_JDBCとMyBatisの課題を提出

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,19 +1,41 @@
 package raisetech.StudentManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-@GetMapping("/hello")
-	public String hello(){
-		return "Hello,World";
-}
+    @Autowired
+    private StudentRepository repository;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
+    }
+
+    @GetMapping("/student")
+    public List<Student>getStudent(){
+        return repository.searchByName();
+    }
+
+    @PostMapping("/student")
+    public void registerStudent(String name, int age) {
+        repository.registerStudent(name, age);
+    }
+
+    @PatchMapping("/student")
+    public void updateStudent(String name, int age) {
+        repository.updateStudent(name, age);
+    }
+
+    @DeleteMapping("/student")
+    public void deleteStudent(String name) {
+        repository.deleteStudent(name);
+    }
 }
