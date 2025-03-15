923cd2e0d38090ab57306b129671a7b4eaf38e8c
Fukuta Haruki <fukutaharuki@fukudaharukaonorenoMacBook-Air.local>
2025-03-16 03:14:57 +0900

    23_MyBatisを使ってCRUDのRead処理を実装と課題提出

--- src/main/java/raisetech/StudentManagement/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -7,15 +7,9 @@ import java.util.List;
 @Mapper
 public interface StudentRepository {
 
-    @Select("SELECT * FROM student")
-    List<Student> searchByName();
+    @Select("SELECT * FROM students")
+    List<Student> search();
 
-    @Insert("INSERT student values(#{name},#{age})")
-    void registerStudent(String name, int age);
-
-    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-    void updateStudent(String name, int age);
-
-    @Delete("DELETE FROM student WHERE name = #{name}")
-    void deleteStudent(String name);
+    @Select("SELECT * FROM students_courses")
+    List<StudentCourses>search2();
 }
